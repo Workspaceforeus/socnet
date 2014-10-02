e121283a23719dffa09476039f8bade13ebae64b
katesh <katescherbak@gmail.com>
2014-10-02 18:32:58 +0300

    Чуть-чуть верстки.

--- app/view/home/1.php
+++ app/view/home/1.php
@@ -1,7 +1,9 @@
 <p>
 <h1> Welcome to our website!</h1>
-<h2><pre>Our site is the only existing social network for people,
+<div class="maininfo"><h2><pre>Our site is the only existing social network for people,
  who like board games!</pre></h2>
+ <p><a href = "http://vk.loc/index.php?r=user&a=registration"> Registration! </a></p>
+ </div>
  <?php include BASE_PATH.'/app/view/user/login.php'; ?>
 
 
