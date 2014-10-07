89fd7444f5dadb62bca5ccf86263ec22b3bd3e4d
icefier <icefier@mail.ru>
2014-10-07 17:30:44 +0400

    Регистратион!

--- app/view/home/1.php
+++ app/view/home/1.php
@@ -2,7 +2,7 @@
 <h1> Welcome to our website!</h1>
 <div class="maininfo"><h2><pre>Our site is the only existing social network for people,
  who like board games!</pre></h2>
- <p><a href = "http://vk.loc/index.php?r=user&a=registration"> Registration! </a></p>
+ <h2><a href = "http://vk.loc/index.php?r=user&a=registration"> Registration! </a></h2>
  </div>
  <?php include BASE_PATH.'/app/view/user/login.php'; ?>
 
